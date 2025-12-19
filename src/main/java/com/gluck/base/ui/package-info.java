01b62557c8043f384cb4848f75b29e3fd8e6e39f
start.vaadin.com <no-reply@vaadin.com>
2025-12-19 11:45:57 +0000

    Generated project

---
+++ src/main/java/com/gluck/base/ui/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package com.gluck.base.ui;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/com/gluck/examplefeature/package-info.java
@@ -0,0 +1,5 @@
+@NullMarked
+package com.gluck.examplefeature;
+// TODO Remove this package once you have added real features
+
+import org.jspecify.annotations.NullMarked;
