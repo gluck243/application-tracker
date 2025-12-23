85a19a35719c30f32fbf7882bc5c8faf2bb13462
gluck243 <gluck243@gmail.com>
2025-12-23 21:37:59 +0200

    test(repo): add repository test

---
+++ src/test/kotlin/com/gluck/jobtracker/ApplicationRepositoryTest.kt
@@ -0,0 +1,31 @@
+package com.gluck.jobtracker
+
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import kotlin.test.assertEquals
+
+@DataJpaTest
+class ApplicationRepositoryTest {
+
+    @Autowired
+    lateinit var repository: ApplicationRepository
+
+    @Test
+    fun `should filter companies by name`() {
+        val record1 = JobApplication(position = "Backend Dev", companyName = "Abc Ltd")
+        val record2 = JobApplication(position = "Programmer", companyName = "Google", status = Status.INTERVIEWING)
+        val record3 = JobApplication(position = "Code Pro", companyName = "Bongo", status = Status.APPLIED)
+
+        repository.save(record1)
+        repository.save(record2)
+        repository.save(record3)
+
+        val filterResult = repository.filterByCompany("goo")
+
+        assertEquals(filterResult.size, 1)
+        assertEquals(filterResult[0].companyName, "Google")
+    }
+
+}
