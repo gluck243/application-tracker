e5bb2edc036c5d791f4cb8b0a8fc44a9546b0933
gluck243 <gluck243@gmail.com>
2025-12-23 13:44:09 +0200

    feat(ui): add dialog to add new records

--- src/main/kotlin/com/gluck/jobtracker/JobListView.kt
+++ src/main/kotlin/com/gluck/jobtracker/JobListView.kt
@@ -1,11 +1,13 @@
 package com.gluck.jobtracker
 
 import com.gluck.base.ui.MainLayout
+import com.vaadin.flow.component.Component
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.dialog.Dialog
 import com.vaadin.flow.component.grid.Grid
 import com.vaadin.flow.component.html.H1
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
-import com.vaadin.flow.component.textfield.TextField
-import com.vaadin.flow.data.binder.Binder
 import com.vaadin.flow.router.Menu
 import com.vaadin.flow.router.Route
 
@@ -15,27 +17,78 @@ class JobListView(private val  repository: ApplicationRepository): VerticalLayou
 
     private val grid = Grid(JobApplication::class.java)
     private val jobForm = JobForm()
-
-    private val binder: Binder<JobApplication> = Binder()
-    private val positionField = TextField()
-    private val companyNameField = TextField()
-    private val statusField = TextField()
+    private val dialog = Dialog("Job Application Information")
 
     init {
         addClassName("list-view")
         setSizeFull()
         configureGrid()
-        add(H1("Job Applications"), jobForm, grid)
+        configureDialog()
+        configureJobForm()
+        add(H1("Job Applications"), getToolbar(), grid)
         updateList()
     }
 
     private fun configureGrid() {
         grid.setColumns("position", "companyName", "status")
         grid.columns.forEach { it.isAutoWidth = true }
+        grid.addItemDoubleClickListener { event ->
+            editJob(event.item)
+        }
+    }
+
+    private fun editJob(job: JobApplication) {
+        if(job == null) {
+            closeEditor()
+        } else {
+            jobForm.setJob(job)
+            dialog.open()
+        }
+    }
+
+    private fun closeEditor() {
+        jobForm.setJob(null)
+        dialog.close()
     }
 
     private fun updateList() {
         grid.setItems(repository.findAll())
+    }
+
+    private fun configureJobForm() {
+        jobForm.addSaveListener { event ->
+            repository.save(event.job)
+            updateList()
+            closeEditor()
+        }
+
+        jobForm.addDeleteListener { event ->
+            repository.delete(event.job)
+            updateList()
+            closeEditor()
+        }
+
+        jobForm.addCancelButton {
+            closeEditor()
+        }
+    }
+
+    private fun configureDialog() {
+        dialog.add(jobForm)
+    }
+
+    private fun getToolbar(): Component {
+        val addJobButton = Button("Add Job")
+        addJobButton.addClickListener {
+            addJob()
+        }
+        return HorizontalLayout(addJobButton)
+    }
+
+    private fun addJob() {
+        grid.asSingleSelect().clear()
+        jobForm.setJob(JobApplication())
+        dialog.open()
     }
 
 }
