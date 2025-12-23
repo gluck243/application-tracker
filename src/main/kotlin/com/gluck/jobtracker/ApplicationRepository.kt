a3c94afb3cbd060ce79864bb1cd2b31557d9e926
gluck243 <gluck243@gmail.com>
2025-12-23 19:24:52 +0200

    chore(repo): add filter by compaby name query method

--- src/main/kotlin/com/gluck/jobtracker/ApplicationRepository.kt
+++ src/main/kotlin/com/gluck/jobtracker/ApplicationRepository.kt
@@ -1,6 +1,9 @@
 package com.gluck.jobtracker
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface ApplicationRepository: JpaRepository<JobApplication, Long> {
+    @Query("SELECT ja FROM JobApplication ja WHERE LOWER(ja.companyName) LIKE LOWER(CONCAT('%', :typedName, '%'))")
+    fun filterByCompany(typedName: String): List<JobApplication>
 }
