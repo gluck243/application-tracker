6b11d609c035a89ab29c9aa490068d113b2c6de1
gluck243 <gluck243@gmail.com>
2025-12-23 21:37:30 +0200

    fix(config): add application file to remote
    Added accidently forgotten application file to remote

---
+++ src/main/kotlin/com/gluck/JobtrackerApplication.kt
@@ -0,0 +1,17 @@
+package com.gluck
+
+import com.vaadin.flow.component.dependency.StyleSheet
+import com.vaadin.flow.theme.lumo.Lumo
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+@StyleSheet(Lumo.STYLESHEET) // Use Aura.STYLESHEET to use Aura instead
+@StyleSheet(Lumo.UTILITY_STYLESHEET)
+@StyleSheet("styles.css") // Your custom styles
+class JobtrackerApplication
+
+    fun main(args: Array<String>) {
+        runApplication<JobtrackerApplication>(*args)
+    }
+
