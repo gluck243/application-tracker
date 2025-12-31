1abbf607a526bd4989615d828152cc6aca73f91a
gluck243 <gluck243@gmail.com>
2025-12-31 17:54:34 +0200

    feat(controller): add get job by id endpoint

--- src/main/kotlin/com/gluck/jobtracker/controllers/JobController.kt
+++ src/main/kotlin/com/gluck/jobtracker/controllers/JobController.kt
@@ -1,5 +1,6 @@
 package com.gluck.jobtracker.controllers
 
+import com.gluck.jobtracker.exception.NoSuchJobFoundException
 import com.gluck.jobtracker.model.JobApplicationEntity
 import com.gluck.jobtracker.model.JobApplicationRequest
 import com.gluck.jobtracker.model.JobApplicationResponse
@@ -7,9 +8,11 @@ import com.gluck.jobtracker.service.JobService
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
@@ -34,6 +37,12 @@ class JobController(private val service: JobService){
             .toUri()
 
         return ResponseEntity<Unit>.created(location).build()
+    }
+
+    @GetMapping("/jobs/{id}")
+    fun getJobById(@PathVariable id: Long): ResponseEntity<JobApplicationResponse> {
+        val foundEntity = service.findJobById(id)
+        return ResponseEntity<JobApplicationResponse>.ok(foundEntity)
     }
 
 }
