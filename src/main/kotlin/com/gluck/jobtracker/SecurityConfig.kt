f91ffeff792e4a826d8b5394c656ea1afc9e3852
gluck243 <gluck243@gmail.com>
2026-01-09 16:04:44 +0200

    chore(security): config basic auth

---
+++ src/main/kotlin/com/gluck/jobtracker/SecurityConfig.kt
@@ -0,0 +1,45 @@
+package com.gluck.jobtracker
+
+import com.gluck.jobtracker.repository.UserRepository
+import com.gluck.jobtracker.service.UserDetailsServiceImpl
+import com.gluck.jobtracker.ui.LoginView
+import com.vaadin.flow.spring.security.VaadinSecurityConfigurer.vaadin
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+
+        http.csrf { it.disable() }
+
+        http.authorizeHttpRequests { c ->
+            c.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+            c.requestMatchers("/api/**").hasRole("ADMIN")
+            c.requestMatchers("/**").permitAll()
+        }
+
+        http.with(vaadin()) { v ->
+            v.loginView(LoginView::class.java)
+        }
+
+        return http.build()
+
+    }
+
+}
