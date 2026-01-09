45bc0909dd48b599cdc2aa8b10e48c4e9e6b34a6
gluck243 <gluck243@gmail.com>
2026-01-09 20:10:15 +0200

    feat(ui): add back button

--- src/main/kotlin/com/gluck/jobtracker/ui/LoginView.kt
+++ src/main/kotlin/com/gluck/jobtracker/ui/LoginView.kt
@@ -1,8 +1,12 @@
 package com.gluck.jobtracker.ui
 
+import com.vaadin.flow.component.UI
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.button.ButtonVariant
 import com.vaadin.flow.component.html.Span
 import com.vaadin.flow.component.login.LoginI18n
 import com.vaadin.flow.component.login.LoginOverlay
+import com.vaadin.flow.component.orderedlayout.FlexComponent
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.router.BeforeEnterEvent
 import com.vaadin.flow.router.BeforeEnterObserver
@@ -38,17 +42,31 @@ class LoginView: VerticalLayout(), BeforeEnterObserver {
             }
         }
 
+        val backButton = Button("<-- Go Back")
+        backButton.apply {
+            addThemeVariants(ButtonVariant.LUMO_SMALL, ButtonVariant.LUMO_CONTRAST)
+            addClickListener {
+                loginOverlay.isOpened = false
+                UI.getCurrent().navigate("")
+            }
+        }
+
         val footerText = Span("Don't have an account? Too bad, it's invite only")
         footerText.apply {
             style.set("font-size", "var(--lumo-font-size-xs)")
             style.set("color", "var(--lumo-secondary-text-color)")
+        }
+        val footerLayout = VerticalLayout(footerText, backButton)
+        footerLayout.apply {
+            alignItems = FlexComponent.Alignment.CENTER
+            isPadding = false
         }
 
         loginOverlay.apply {
             setTitle("Job Application Tracker")
             description = "Please log in to manage your applications."
             setI18n(i18n)
-            footer.add(footerText)
+            footer.add(footerLayout)
             isForgotPasswordButtonVisible = false
             isOpened = true
             action = "login"
