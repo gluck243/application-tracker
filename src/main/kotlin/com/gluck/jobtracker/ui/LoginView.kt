696b1dc5e5d81999289aa5dfd97b112f121a5503
gluck243 <gluck243@gmail.com>
2026-01-09 16:07:22 +0200

    feat(ui): add login view

---
+++ src/main/kotlin/com/gluck/jobtracker/ui/LoginView.kt
@@ -0,0 +1,29 @@
+package com.gluck.jobtracker.ui
+
+import com.vaadin.flow.component.login.LoginOverlay
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.router.BeforeEnterEvent
+import com.vaadin.flow.router.BeforeEnterObserver
+import com.vaadin.flow.router.Route
+import com.vaadin.flow.server.auth.AnonymousAllowed
+
+@Route("login")
+@AnonymousAllowed
+class LoginView: VerticalLayout(), BeforeEnterObserver {
+
+    val loginOverlay = LoginOverlay()
+
+    override fun beforeEnter(event: BeforeEnterEvent) {
+        val params = event.location.queryParameters.parameters
+        if (params.containsKey("error"))
+            loginOverlay.isError = true
+    }
+
+    init {
+        loginOverlay.apply {
+            isOpened = true
+            action = "login"
+        }
+    }
+
+}
