885011df55021fb7b42f2cb8d89c32d1f1826342
gluck243 <gluck243@gmail.com>
2026-01-16 22:05:07 +0200

    docs(controller): add docs to REST endpoints

--- src/main/kotlin/com/gluck/jobtracker/controllers/JobController.kt
+++ src/main/kotlin/com/gluck/jobtracker/controllers/JobController.kt
@@ -1,8 +1,14 @@
 package com.gluck.jobtracker.controllers
 
+import com.gluck.jobtracker.model.ApiErrorResponse
 import com.gluck.jobtracker.model.JobApplicationRequest
 import com.gluck.jobtracker.model.JobApplicationResponse
 import com.gluck.jobtracker.service.JobService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -17,14 +23,32 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
 @RestController
 @RequestMapping("/api")
-class JobController(private val service: JobService){
+class JobController(private val service: JobService) {
 
+    @Operation(
+        summary = "Get All Job Applications",
+        description = "Lists all available jobs applications"
+    )
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Job Applications Found"),
+    ])
     @GetMapping("/jobs")
     fun getAllApplications(): ResponseEntity<List<JobApplicationResponse>> {
         val applications = service.getAllJobs()
         return ResponseEntity.ok(applications)
     }
 
+    @Operation(
+        summary = "Add a new Job Application Record",
+        description = "Add information about a new job application. Make sure information is entered in a correct format",
+    )
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "201", description = "Job Applications created", content = [
+            Content(mediaType = "text/plain", schema =
+                Schema(type = "string", example = "/api/jobs/3"))]),
+        ApiResponse(responseCode = "400", description = "Check request correctness"),
+        ApiResponse(responseCode = "403", description = "Unauthorized, login to process request")
+    ])
     @PostMapping("/jobs")
     fun createJob(@RequestBody @Valid dto: JobApplicationRequest): ResponseEntity<Unit> {
         val newId = service.saveJob(dto)
@@ -38,18 +62,47 @@ class JobController(private val service: JobService){
         return ResponseEntity.created(location).build()
     }
 
+    @Operation(
+        summary = "Get a Job Application By Id",
+        description = "Lists a Job Application found by valid id"
+    )
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Job Application by provided id found", content = [
+            Content(mediaType = "application/json",
+                schema = Schema(implementation = JobApplicationResponse::class))
+        ]),
+        ApiResponse(responseCode = "404", description = "Job Application by provided id not found", content = [
+            Content(mediaType = "application/json",
+                schema = Schema(implementation = ApiErrorResponse::class))
+        ])
+    ])
     @GetMapping("/jobs/{id}")
     fun getJobById(@PathVariable id: Long): ResponseEntity<JobApplicationResponse> {
         val foundEntity = service.findJobById(id)
         return ResponseEntity.ok(foundEntity)
     }
 
+    @Operation(
+        summary = "Update a Job Application By Id",
+        description = "Updates a Job Application by a valid id. Id and Request Body must be valid"
+    )
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Job Application by provided id is found and updated",
+            content = [Content(mediaType = "application/json",
+                schema = Schema(implementation = JobApplicationResponse::class))
+            ]),
+        ApiResponse(responseCode = "404", description = "Job application by provided id is not found and wasn't updated",
+            content = [Content(mediaType = "application/json",
+                schema = Schema(implementation = ApiErrorResponse::class))]),
+        ApiResponse(responseCode = "400", description = "Bad request. Check correctness of provided request")
+    ])
     @PutMapping("/jobs/{id}")
     fun updateJobById(@PathVariable id: Long, @RequestBody @Valid dto: JobApplicationRequest): ResponseEntity<JobApplicationResponse> {
         val updatedEntity = service.updateJobById(id, dto)
         return ResponseEntity.ok(updatedEntity)
     }
 
+    // Add documentation
     @DeleteMapping("jobs/{id}")
     fun deleteJobById(@PathVariable id: Long): ResponseEntity<Unit> {
         service.deleteJob(id)
