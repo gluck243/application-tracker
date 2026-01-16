2598fe53ca54cc602213b7ededeee76a5478deca
gluck243 <gluck243@gmail.com>
2026-01-16 18:59:46 +0200

    chore(security): add cors config to security chain

--- src/main/kotlin/com/gluck/jobtracker/SecurityConfig.kt
+++ src/main/kotlin/com/gluck/jobtracker/SecurityConfig.kt
@@ -1,7 +1,5 @@
 package com.gluck.jobtracker
 
-import com.gluck.jobtracker.repository.UserRepository
-import com.gluck.jobtracker.service.UserDetailsServiceImpl
 import com.gluck.jobtracker.ui.LoginView
 import com.vaadin.flow.spring.security.VaadinSecurityConfigurer.vaadin
 import org.springframework.context.annotation.Bean
@@ -9,10 +7,10 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -24,12 +22,16 @@ class SecurityConfig {
     }
 
     @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+    fun securityFilterChain(http: HttpSecurity, corsSource: CorsConfigurationSource): SecurityFilterChain {
+
+        http.cors { c ->
+            c.configurationSource(corsSource)
+        }
 
         http.csrf { it.disable() }
 
         http.authorizeHttpRequests { c ->
-            c.requestMatchers(HttpMethod.GET, "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+            c.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
             c.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
             c.requestMatchers("/api/**").hasRole("ADMIN")
             c.requestMatchers("/**").permitAll()
