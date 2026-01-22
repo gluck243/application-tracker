4d3c96b44f12cb7b174e923253404d80d9c67195
gluck243 <gluck243@gmail.com>
2026-01-22 12:39:13 +0200

    feat(db): add pagination to queries

--- src/main/kotlin/com/gluck/jobtracker/repository/ApplicationRepository.kt
+++ src/main/kotlin/com/gluck/jobtracker/repository/ApplicationRepository.kt
@@ -1,10 +1,18 @@
 package com.gluck.jobtracker.repository
 
 import com.gluck.jobtracker.model.JobApplicationEntity
+import com.gluck.jobtracker.model.Status
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface ApplicationRepository: JpaRepository<JobApplicationEntity, Long> {
     @Query("SELECT ja FROM JobApplicationEntity ja WHERE LOWER(ja.companyName) LIKE LOWER(CONCAT('%', :typedName, '%'))")
-    fun filterByCompany(typedName: String): List<JobApplicationEntity>
+    fun searchByCompany(typedName: String, pageable: Pageable): Page<JobApplicationEntity>
+
+    fun countByCompanyNameContainsIgnoreCase(companyName: String): Long
+
+//    @Query("SELECT ja.status, COUNT(ja.status) FROM JobApplicationEntity ja GROUP BY ja.status ORDER BY COUNT(ja.status)")
+//    fun countAllByStatus(): HashMap<Status, Int>
 }
