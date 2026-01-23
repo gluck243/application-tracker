50da439178d669e6650afb1fc619862f2dc28a14
gluck243 <gluck243@gmail.com>
2026-01-23 19:54:43 +0200

    feat(db): add new repo functions

--- src/main/kotlin/com/gluck/jobtracker/repository/ApplicationRepository.kt
+++ src/main/kotlin/com/gluck/jobtracker/repository/ApplicationRepository.kt
@@ -8,10 +8,17 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface ApplicationRepository: JpaRepository<JobApplicationEntity, Long> {
+    @Query("SELECT ja FROM JobApplicationEntity ja WHERE LOWER(ja.position) LIKE LOWER(CONCAT('%', :typedName, '%'))")
+    fun searchByPosition(typedName: String, pageable: Pageable): Page<JobApplicationEntity>
     @Query("SELECT ja FROM JobApplicationEntity ja WHERE LOWER(ja.companyName) LIKE LOWER(CONCAT('%', :typedName, '%'))")
     fun searchByCompany(typedName: String, pageable: Pageable): Page<JobApplicationEntity>
+    @Query("SELECT ja FROM JobApplicationEntity ja WHERE LOWER(CAST(ja.description AS STRING)) LIKE LOWER(CONCAT('%', :typedName, '%'))")
+    fun searchByDescription(typedName: String, pageable: Pageable): Page<JobApplicationEntity>
 
+    fun countByPositionContainsIgnoreCase(position: String): Long
     fun countByCompanyNameContainsIgnoreCase(companyName: String): Long
+    @Query("SELECT COUNT(ja) FROM JobApplicationEntity ja WHERE UPPER(CAST(ja.description AS STRING)) LIKE UPPER(CONCAT('%', :desc, '%'))")
+    fun countByDescriptionContainsIgnoreCase(desc: String): Long
 
 //    @Query("SELECT ja.status, COUNT(ja.status) FROM JobApplicationEntity ja GROUP BY ja.status ORDER BY COUNT(ja.status)")
 //    fun countAllByStatus(): HashMap<Status, Int>
