aeec6bb12803988a32b79f9092af65049c434953
gluck243 <gluck243@gmail.com>
2026-01-29 17:49:34 +0200

    refactor(db): recreated user repo in java

---
+++ src/main/java/com/gluck/jobtracker/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.gluck.jobtracker.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
+}
+
