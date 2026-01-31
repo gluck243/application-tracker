fab3dd7295621eba69d42d78306394ac6acb2048
gluck243 <gluck243@gmail.com>
2026-01-31 13:00:59 +0200

    refactor: remove redundant kotlin files

--- src/main/kotlin/com/gluck/jobtracker/exception/NoSuchJobFoundException.kt
+++
@@ -1,3 +0,0 @@
-package com.gluck.jobtracker.exception
-
-class NoSuchJobFoundException(override val message: String = "No matching job found!"): RuntimeException()
--- src/main/kotlin/com/gluck/jobtracker/model/JobFilter.kt
+++
@@ -1,6 +0,0 @@
-package com.gluck.jobtracker.model
-
-data class JobFilter(
-    var searchTerm: String? = null,
-    var searchBy: String = "Company"
-)
