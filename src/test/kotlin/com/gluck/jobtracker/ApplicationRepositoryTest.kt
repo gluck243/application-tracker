f83c9c2a17b05661ebb8d64b77f34f58a1a347de
gluck243 <gluck243@gmail.com>
2026-02-03 12:10:53 +0200

    chore(test): expand repo test coverage

--- src/test/kotlin/com/gluck/jobtracker/ApplicationRepositoryTest.kt
+++ src/test/kotlin/com/gluck/jobtracker/ApplicationRepositoryTest.kt
@@ -1,10 +1,11 @@
 package com.gluck.jobtracker
 
 
-import com.gluck.jobtracker.repository.JobApplicationEntity
 import com.gluck.jobtracker.model.Status
 import com.gluck.jobtracker.repository.ApplicationRepository
+import com.gluck.jobtracker.repository.JobApplicationEntity
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest
 import org.springframework.data.domain.PageRequest
@@ -17,18 +18,92 @@ class ApplicationRepositoryTest {
     lateinit var repository: ApplicationRepository
 
     @Test
-    fun `should filter companies by name`() {
-        val record1 = JobApplicationEntity(0L, "Backend Dev", "Abc Ltd", Status.APPLIED, null, "")
-        val record2 = JobApplicationEntity(0L, "Programmer", "Google", Status.INTERVIEWING, null, "")
-        val record3 = JobApplicationEntity(0L, "Code Pro", "Bongo", Status.APPLIED, null, "")
+    fun `should save and filter companies by name`() {
 
-        repository.saveAll(listOf(record1, record2, record3))
+        repository.saveAll(getRecords())
 
         val filterResult = repository.searchByCompany("goo", PageRequest.of(0, 10))
 
         assertEquals(1, filterResult.totalElements)
 
         assertEquals("Google", filterResult.content[0].companyName)
+    }
+
+    @Test
+    fun `should save and count total jobs as 3`() {
+
+        repository.saveAll(getRecords())
+
+        val allJobs = repository.count()
+
+        assertEquals(3, allJobs)
+
+    }
+
+    @Test
+    fun `should save and count applied jobs as 2 and interviewing as 1`() {
+
+        repository.saveAll(getRecords())
+
+        val applied = repository.countByStatusIs(Status.APPLIED)
+        val interviewing = repository.countByStatusIs(Status.INTERVIEWING)
+
+        assertEquals(2, applied)
+        assertEquals(1, interviewing)
+
+    }
+
+    @Test
+    fun `should save and filter companies by position`() {
+
+        repository.saveAll(getRecords())
+
+        val filterResult = repository.searchByPosition("pro", PageRequest.of(0, 10))
+
+        assertEquals(2, filterResult.totalElements)
+
+        assertEquals("Programmer", filterResult.content[0].position)
+        assertEquals("Code Pro", filterResult.content[1].position)
+
+    }
+
+    @Test
+    fun `should save and filter companies by description`() {
+
+        repository.saveAll(getRecords())
+
+        val filterResult = repository.searchByDescription("test", PageRequest.of(0, 10))
+
+        assertEquals(3, filterResult.totalElements)
+
+    }
+
+    private fun getRecords(): List<JobApplicationEntity> {
+        return listOf(
+            JobApplicationEntity(
+                0L,
+                "Backend Dev",
+                "Abc Ltd",
+                Status.APPLIED, null,
+                "Testing"
+            ),
+            JobApplicationEntity(
+                0L,
+                "Programmer",
+                "Google",
+                Status.INTERVIEWING,
+                null,
+                "I want to write a test"
+            ),
+            JobApplicationEntity(
+                0L,
+                "Code Pro",
+                "Bongo",
+                Status.APPLIED,
+                null,
+                "Test"
+            )
+        )
     }
 
 }
